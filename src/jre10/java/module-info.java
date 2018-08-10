3967f2794fabeb2e1d31d90a96f141fa0aa7ab62
GedMarc <ged_marc@hotmail.com>
2018-08-10 10:46:06 +0200

    Plugin POM updates for JRE10, and maven dependency classification bug fix

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -9,8 +9,6 @@ module com.jwebmp.plugins.weblogappender {
 
 	requires java.validation;
 	requires java.logging;
-	requires com.jwebmp.plugins.atmosphere;
-	requires atmosphere.runtime;
 
 	opens com.jwebmp.plugins.weblogappender to com.fasterxml.jackson.databind,com.jwebmp.core;
 	opens com.jwebmp.plugins.weblogappender.annotations to com.fasterxml.jackson.databind,com.jwebmp.core;
